fa09c9909605ca12e092bdbe5a91a2555fe298c9
neilfr <neilfr@live.com>
2024-07-23 16:17:22 -0400

    add API setup, with Sanctum, update User and Sanctum tokens to utilize UUIDs for ids.

---
+++ app/Models/PersonalAccessToken.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
+
+class PersonalAccessToken extends SanctumPersonalAccessToken
+{
+    use HasFactory;
+
+    // $keyType, $incrementing and boot() added below to support UUID instead of auto-incrementing integer for tokens
+    public $keyType = 'string';
+
+    public $incrementing = false;
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+
+    // $keyType and $incrementing required below to support using UUIDs as primary key in users table
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -31,6 +38,12 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public static function booted() {
+        static::creating(function ($user) {
+            $user->id = Str::uuid();
+        });
+    }
 
     /**
      * Get the attributes that should be cast.
