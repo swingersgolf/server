fcea3b3d38168fb8682396824997f5f4a4f8a8aa
neilfr <neilfr@live.com>
2024-07-23 21:12:30 -0400

    registration and login validation testing

--- tests/Feature/Api/Controllers/AuthControllerTest.php
+++ tests/Feature/Api/Controllers/AuthControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Api\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
@@ -16,11 +17,100 @@ class AuthControllerTest extends TestCase
             'password' => 'password',
         ];
 
-        $response = $this->post('/api/register', $userPayload)
+        $this->post('/api/register', $userPayload)
             ->assertCreated();
 
         unset($userPayload['password']);
 
         $this->assertDatabaseHas('users', $userPayload);
     }
+
+    #[DataProvider('loginPayloads')] public function test_login_validates_payload($payload, $error):void
+    {
+
+        $this->post('/api/login', $payload)
+            ->assertSessionHasErrors($error);
+    }
+
+    #[DataProvider('registrationPayloads')] public function test_registration_validates_payload($payload, $error):void
+    {
+
+        $response = $this->post('/api/register', $payload)
+            ->assertSessionHasErrors($error);
+    }
+
+    public static function registrationPayloads()
+    {
+        return[
+            [
+                'payload' => [
+                    'email' => 'my.name@example.com',
+                    'password' => 'password',
+                ],
+                'error' => 'name',
+            ],
+            [
+                'payload' => [
+                    'name' => 'my name',
+                    'password' => 'password',
+                ],
+                'error' => 'email',
+            ],
+            [
+                'payload' => [
+                    'name' => 'my name',
+                    'email' => 'my.name@example.com',
+                ],
+                'error' => 'password',
+            ],
+            [
+                'payload' => [
+                    'name' => 'my name',
+                    'email' => 'my.name@example.com',
+                    'password' => 'passwor',
+                ],
+                'error' => 'password',
+            ],
+            [
+                'payload' => [
+                    'name' => 'my name',
+                    'email' => 'not an email',
+                    'password' => 'password',
+                ],
+                'error' => 'email',
+            ]
+        ];
+    }
+    public static function loginPayloads()
+    {
+        return[
+            [
+                'payload' => [
+                    'password' => 'password',
+                ],
+                'error' => 'email',
+            ],
+            [
+                'payload' => [
+                    'email' => 'my.name@example.com',
+                ],
+                'error' => 'password',
+            ],
+            [
+                'payload' => [
+                    'email' => 'my.name@example.com',
+                    'password' => 'passwor',
+                ],
+                'error' => 'password',
+            ],
+            [
+                'payload' => [
+                    'email' => 'not an email',
+                    'password' => 'password',
+                ],
+                'error' => 'email',
+            ]
+        ];
+    }
+
 }
