d07a9a2b6aabbc514435acd5486140d259a99095
neilfr <neilfr@live.com>
2024-08-05 21:53:15 -0400

    return validated token for google authenticated account

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -3,34 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Traits\ApiResponses;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleLoginController extends Controller
 {
+    use ApiResponses;
+
     public function redirectToGoogle()
     {
         return Socialite::driver('google')->redirect();
     }
 
-
     public function handleGoogleCallback()
     {
         $googleUser = Socialite::driver('google')->user();
         $user = User::where('email', $googleUser->email)->first();
-        if(!$user)
-        {
+        if (! $user) {
             $user = User::create([
                 'name' => $googleUser->name,
                 'email' => $googleUser->email,
-                'password' => Hash::make(Str::random(16))
+                'password' => Hash::make(Str::random(16)),
             ]);
         }
 
-        $token = $user->createToken('API Token for Google user ' . $user->email)->plainTextToken;
-
-        // TODO: update below to the correct front end route.
-        return view('welcome', ['token' => $token]);
+        return $this->ok('Authenticated',
+            [
+                'token' => $user->createToken('API Token for Google user '.$user->email)->plainTextToken,
+            ]);
     }
 }
