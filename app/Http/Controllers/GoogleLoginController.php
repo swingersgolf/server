0045fbb50bacc12b3643728eafe02b3501f80244
neilfr <neilfr@live.com>
2024-08-17 14:54:09 -0400

    try stateless fix

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -19,7 +19,7 @@ class GoogleLoginController extends Controller
 
     public function handleGoogleCallback()
     {
-        $googleUser = Socialite::driver('google')->user();
+        $googleUser = Socialite::driver('google')->stateless()->user();
         $user = User::where('email', $googleUser->email)->first();
         if (! $user) {
             $user = User::create([
