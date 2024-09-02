5347006ab66d18208c303d12a7befea5eb6534fc
neilfr <neilfr@live.com>
2024-09-02 16:09:43 -0400

    validate user profile update

--- tests/Feature/Controllers/Api/V1/UserProfileControllerTest.php
+++ tests/Feature/Controllers/Api/V1/UserProfileControllerTest.php
@@ -35,6 +35,45 @@ class UserProfileControllerTest extends TestCase
         $this->assertDatabaseHas('user_profiles', $expectation);
     }
 
+    #[DataProvider('invalidPayloads')]
+    public function test_it_returns_errors_for_invalid_user_profile_payloads($payload, $error): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)->patch(route('api.v1.user-profile.update'), $payload)
+            ->assertSessionHasErrors($error);
+    }
+
+    public static function invalidPayloads():array
+    {
+        return [
+            'handicap over' => [
+                'payload' => ['handicap' => '55'],
+                'error' => 'handicap'
+            ],
+            'handicap under' => [
+                'payload' => ['handicap' => '-55'],
+                'error' => 'handicap'
+            ],
+            'handicap not a number' => [
+                'payload' => ['handicap' => 'foobar'],
+                'error' => 'handicap'
+            ],
+            'invalid Canadian postal code' => [
+                'payload' => ['postalCode' => 'ABCDEF'],
+                'error' => 'postal_code'
+            ],
+            'invalid American zip code' => [
+                'payload' => ['postalCode' => '987654'],
+                'error' => 'postal_code'
+            ],
+            'invalid American long zip code' => [
+                'payload' => ['postalCode' => '90210-12345'],
+                'error' => 'postal_code'
+            ],
+        ];
+    }
+
     public static function validPayloads():array
     {
         return [
@@ -42,9 +81,17 @@ class UserProfileControllerTest extends TestCase
                 'payload' => ['handicap' => '12'],
                 'expectation' => ['handicap' => '12'],
             ],
-            'postalCode' => [
+            'Canadian postalCode' => [
                 'payload' => ['postalCode' => 'H0H0H0'],
                 'expectation' => ['postal_code' => 'H0H0H0'],
+            ],
+            'American zip' => [
+                'payload' => ['postalCode' => '90210'],
+                'expectation' => ['postal_code' => '90210'],
+            ],
+            'American long zip' => [
+                'payload' => ['postalCode' => '90210-1234'],
+                'expectation' => ['postal_code' => '90210-1234'],
             ]
         ];
     }
