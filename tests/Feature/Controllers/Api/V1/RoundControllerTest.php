59b16222e3066e7e231cdeeaa8b52ad8ee83e680
neilfr <neilfr@live.com>
2024-09-22 22:11:57 -0400

    show route returns round details

--- tests/Feature/Controllers/Api/V1/RoundControllerTest.php
+++ tests/Feature/Controllers/Api/V1/RoundControllerTest.php
@@ -53,5 +53,38 @@ class RoundControllerTest extends TestCase
 
         $this->assertCount($users->count(), $responseData['users']);
     }
+    public function test_show_returns_round(): void
+    {
+        $users = User::factory()->count(3)->create();
+        $where = Course::factory()->create([
+            'name' => 'Some Course Name',
+        ]);
+        $attributes = Attribute::factory()->count(3)->create();
 
+        $round = Round::factory()->create([
+            'when' => now(),
+            'course_id' => $where->id,
+        ]);
+
+        $round->attributes()->attach($attributes);
+        $round->users()->attach($users);
+
+        $response = $this->actingAs($users[0])->get(route('api.v1.round.show', $round->id))
+            ->assertSuccessful();
+        $responseData = $response->json()['data'];
+
+        $this->assertEquals($round->when, $responseData['when']);
+
+        $courseName = Course::find($round->course_id)->name;
+        $this->assertEquals($courseName, $responseData['course']);
+
+        $this->assertCount($attributes->count(), $responseData['attributes']);
+        $attributes->map(function ($attribute) use ($responseData) {
+            return in_array($attribute->name, $responseData['attributes']) &&
+                in_array($attribute->id, $responseData['attributes']);
+        });
+        $this->assertCount($attributes->count(), $responseData['attributes']);
+
+        $this->assertCount($users->count(), $responseData['users']);
+    }
 }
