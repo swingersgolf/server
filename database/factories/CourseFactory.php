768199ae3f4db54cee82fc52cab76d75298a23db
Aidan Froggatt <aidanfr@live.ca>
2024-10-27 16:47:55 -0400

    updated controller ot use factory

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CourseFactory extends Factory
 {
+    protected $model = Course::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,8 @@ class CourseFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'course_name' => $this->faker->name(),
+            'city_name' => $this->faker->city(),
         ];
     }
 }
