c9933ff2d9f3d04d0d84465c95d22521bdaef453
Aidan Froggatt <aidanfr@live.ca>
2024-11-01 20:07:14 -0400

    added files for notifications on server

---
+++ app/Http/Resources/Api/V1/NotificationResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'user_id' => $this->user_id,
+            'data' => $this->data,
+            'read_at' => $this->read_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
