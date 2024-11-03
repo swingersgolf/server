f0247b7adf48a820a305d5b027fab026079ad9d9
Aidan Froggatt <aidanfr@live.ca>
2024-11-03 12:24:42 -0500

    validation for updating uesr

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\UserUpdateRequest;
 use App\Http\Resources\Api\V1\UserResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -15,7 +16,7 @@ class UserController extends Controller
         return new UserResource(User::find(Auth::id()));
     }
 
-    public function update(Request $request) 
+    public function update(UserUpdateRequest $request) 
     {
         $user = User::find(Auth::id());
         if (!$user) {
