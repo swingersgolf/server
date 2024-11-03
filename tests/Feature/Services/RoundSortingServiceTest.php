63c65ce5e396b610429874fc1127007c2d48a3e9
neilfr <neilfr@live.com>
2024-11-03 09:02:29 -0500

    first try at sorting by preferences

--- tests/Feature/Services/RoundSortingServiceTest.php
+++ tests/Feature/Services/RoundSortingServiceTest.php
@@ -2,16 +2,20 @@
 
 namespace Tests\Feature\Services;
 
+use App\Models\Preference;
 use App\Models\Round;
+use App\Models\User;
 use App\Services\RoundSorting\SortByCreateStrategy;
 use App\Services\RoundSorting\RoundSortingService;
+use App\Services\RoundSorting\SortByPreferencesStrategy;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class RoundSortingServiceTest extends TestCase
 {
-    public function test_it_sorts_rounds(): void
+    public function test_it_sorts_rounds_by_create_dated(): void
     {
         $sortingStrategy = new SortByCreateStrategy();
         $roundSortingService = new RoundSortingService($sortingStrategy);
@@ -35,5 +39,32 @@ class RoundSortingServiceTest extends TestCase
         $this->assertEquals($sortedRounds[0]->id, $round1->id);
         $this->assertEquals($sortedRounds[1]->id, $round3->id);
         $this->assertEquals($sortedRounds[2]->id, $round2->id);
+    }
+
+    public function test_it_sorts_rounds_by_preference_match(): void
+    {
+        $sortingStrategy = new SortByPreferencesStrategy();
+        $roundSortingService = new RoundSortingService($sortingStrategy);
+
+        $preference = Preference::factory()->create([
+            'name' => 'one',
+        ]);
+        $round1 = Round::factory()->create([]);
+        $round1->preferences()->attach($preference,['status'=>'disliked']);
+        $round2 = Round::factory()->create([]);
+        $round2->preferences()->attach($preference,['status'=>'preferred']);
+        $round3 = Round::factory()->create([]);
+        $round3->preferences()->attach($preference,['status'=>'indifferent']);
+
+        $rounds = collect([$round1, $round2, $round3]);
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $sortedRounds = $roundSortingService->sortRounds($rounds);
+
+        $this->assertEquals($sortedRounds[0]->id, $round2->id);
+        $this->assertEquals($sortedRounds[1]->id, $round3->id);
+        $this->assertEquals($sortedRounds[2]->id, $round1->id);
     }
 }
