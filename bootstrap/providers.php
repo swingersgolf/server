0741dacc4f14ef0ccf86144f70745f395dc8da6b
Aidan Froggatt <aidanfr@live.ca>
2024-12-01 14:41:10 -0500

    merging again

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-<<<<<<< HEAD
     App\Providers\RoundSortingServiceProvider::class,
-=======
->>>>>>> 401883b04e9fcdcd44adc1838cbfd6b8fc853d32
 ];
