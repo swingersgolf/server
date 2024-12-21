f7b50afa27783c3f6fdd72ac08dd50d4b272fcb6
neilfr <neilfr@live.com>
2024-12-21 17:00:19 -0500

    wip - image upload

--- app/Http/Controllers/Api/V1/ProfilePhotoController.php
+++ app/Http/Controllers/Api/V1/ProfilePhotoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Aws\S3\S3Client;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -10,12 +11,108 @@ use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ProfilePhotoController extends Controller
 {
+    private $s3Client;
+
+    private $bucket;
+
+    public function __construct()
+    {
+        $this->s3Client = new S3Client([
+            'region' => config('filesystems.disks.s3.region'),
+            'version' => 'latest',
+            'credentials' => [
+                'key' => config('filesystems.disks.s3.key'),
+                'secret' => config('filesystems.disks.s3.secret'),
+            ],
+        ]);
+
+        $this->bucket = config('filesystems.disks.s3.bucket');
+    }
+
+    public function get(Request $request): JsonResponse
+    {
+        $request->validate([
+            'file_name' => 'required|string',
+        ]);
+
+        $filePath = 'profile_photos/'.$request->file_name;
+
+        $command = $this->s3Client->getCommand('GetObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $filePath,
+        ]);
+
+        $presignedUrl = $this->s3Client->createPresignedRequest($command, '+20 minutes')->getUri();
+
+        return response()->json(['url' => (string) $presignedUrl]);
+    }
+
+    public function put(Request $request): JsonResponse
+    {
+        $request->validate([
+            'file_name' => 'required|string',
+            'file_type' => 'required|string',
+        ]);
+
+        $filePath = 'profile_photos/'.$request->file_name;
+
+        $command = $this->s3Client->getCommand('PutObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $filePath,
+            'ContentType' => $request->file_type,
+        ]);
+
+        $presignedUrl = $this->s3Client->createPresignedRequest($command, '+20 minutes')->getUri();
+
+        return response()->json(['url' => (string) $presignedUrl]);
+    }
+
+    public function foo(Request $request): JsonResponse
+    {
+        $request->validate([
+            'file_name' => 'required|string',
+            'file_type' => 'required|string',
+            'operation' => 'required|in:get,put',
+        ]);
+
+        $bucket = config('filesystems.disks.s3.bucket');
+        $filePath = 'profile_photos/'.$request->file_name;
+
+        $s3Client = new S3Client([
+            'region' => config('filesystems.disks.s3.region'),
+            'version' => 'latest',
+            'credentials' => [
+                'key' => config('filesystems.disks.s3.key'),
+                'secret' => config('filesystems.disks.s3.secret'),
+            ],
+        ]);
+
+        $commandOptions = [
+            'Bucket' => $bucket,
+            'Key' => $filePath,
+            'ContentType' => $request->file_type,
+        ];
+
+        if ($request->operation === 'put') {
+            $commandOptions['ACL'] = 'public-read'; // Set as required
+        }
+
+        $command = $s3Client->getCommand(
+            $request->operation === 'put' ? 'PutObject' : 'GetObject',
+            $commandOptions
+        );
+
+        $presignedUrl = $s3Client->createPresignedRequest($command, '+20 minutes')->getUri();
+
+        return response()->json(['url' => (string) $presignedUrl]);
+    }
+
     public function generateUploadUrl(Request $request): JsonResponse
     {
         $user = $request->user();
 
         // Define the S3 file path for the user's photo
-        $fileName = "profile-photos/{$user->id}/".uniqid().'.jpg';
+        $fileName = 'test.jpg';
         $disk = Storage::disk('s3');
 
         // Generate a pre-signed URL for upload
