d0419f97563d9df4aa82d4de226512ee85f1c562
Neil Froggatt <neilfr@live.com>
2025-01-29 22:50:42 -0500

    send just user id, first and last name (#47)

--- app/Http/Controllers/Api/V1/MessageController.php
+++ app/Http/Controllers/Api/V1/MessageController.php
@@ -6,9 +6,11 @@ use App\Events\MessageEvent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\MessageStoreRequest;
 use App\Http\Requests\MessageIndexRequest;
+use App\Http\Resources\Api\V1\MessageResource;
 use App\Models\Message;
 use App\Models\MessageGroup;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -35,14 +37,14 @@ class MessageController extends Controller
         return response()->json($message);
     }
 
-    public function index(MessageIndexRequest $request): JsonResponse
+    public function index(MessageIndexRequest $request): AnonymousResourceCollection
     {
         $messageGroup = MessageGroup::findOrFail($request->message_group_id);
         if ($request->user()->cannot('view', $messageGroup)) {
             abort(403);
         }
 
-        $messages = Message::where('message_group_id', $request->message_group_id)->get();
-        return response()->json($messages);
+        $messages = Message::where('message_group_id', $request->message_group_id)->with('user')->get();
+        return MessageResource::collection($messages);
     }
 }
